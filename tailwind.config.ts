f4755217d7935d21ddc64dfa12f964aca22192ab
Jose David Gale <josedavidgale@gmail.com>
2023-09-25 14:45:17 +0100

    adding color theme and navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,18 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: {
+        light: "#64CCC5",
+        DEFAULT: "#64CCC5",
+        dark: "#64CCC5",
+      },
+      secondary: {
+        light: "#001C30",
+        DEFAULT: "#001C30",
+        dark: "#001C30",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
